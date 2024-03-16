0f03654ebf9c0fa482f8e4e0c56fb77b3c6b0726
Hong Beomsun <ghsqjatns@gmail.com>
2024-03-16 15:36:02 +0900

    Update next.config.mjs with new output option

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "i.imgur.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  }
+    output: "export",
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "i.imgur.com",
+                port: "",
+                pathname: "/**",
+            },
+        ],
+    }
 };
 
 export default nextConfig;
