e040acf07bc6658dacdd1800f2087d40af24a000
Hong Beomsun <ghsqjatns@gmail.com>
2024-03-16 18:48:48 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
     output: "export",
+};
+
+const nextConfig = {
     images: {
         remotePatterns: [
             {
